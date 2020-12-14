7c4c344c927cc76ea158effa08314121a28314a1
joackim.orciere <joackim.orciere@alumni.univ-avignon.fr>
2020-12-14 16:00:17 +0100

    Implemented class Tile

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,4 +1,17 @@
 package java;
 
-public class Tile {
+// TODO remove enum, replace with class Player
+enum Player
+{}
+
+public abstract class Tile {
+    private int x;
+    private int y;
+    private char ASCII;
+
+    public int getX() { return x ;}
+    public int getY() { return y ;}
+    abstract void enterTile( Player player );
+    abstract void accessible( Player player );
+    abstract void getASCII();
 }
