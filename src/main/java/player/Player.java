b5fa7562911e5cb0f6c13df431091ce5f65a0a3c
Thomas LEGRAND <thomas.legrand@alumni.univ-avignon.fr>
2020-12-14 16:17:59 +0100

    Implémentation Getters Player

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -4,7 +4,7 @@ public abstract class Player {
     private int x;
     private int y;
     protected int health;
-    protected int hydratation;
+    protected int hydration;
     protected int satiety;
     protected int morale;
     private String pseudo;
@@ -12,11 +12,36 @@ public abstract class Player {
     protected boolean driver = true;
     protected boolean swimsuit = false;
     protected int fines = 0;
-    public boolean alive = true;
+    protected boolean alive = true;
 
     protected Player(int x, int y, String pseudo) {
         this.x = x;
         this.y = y;
         this.pseudo = pseudo;
     }
+
+    //Getters
+    public int getX(){ return this.x;}
+
+    public int getY(){ return this.y;}
+
+    public int getHealth(){ return this.health;}
+
+    public int getHydration(){ return this.hydration;}
+
+    public int getSatiety(){ return this.satiety;}
+
+    public int getMorale(){ return this.morale;}
+
+    public String getPseudo(){ return this.pseudo;}
+
+    public int getNbrDiploma(){ return this.nbrDiploma;}
+
+    public boolean getDriver(){ return this.driver;}
+
+    public boolean getSwimsuit(){ return this.swimsuit;}
+
+    public int getFines(){ return this.fines;}
+
+    public boolean getAlive(){ return this.alive;}
 }
